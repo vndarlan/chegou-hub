a9a279ca8f2bead8a34a8ae0df80f85d6a80f7b4
Vinícius Darlan <viniciuschegouoperacional@gmail.com>
2025-07-31 22:39:24 -0300

    s

--- frontend/src/components/ui/popover.jsx
+++ frontend/src/components/ui/popover.jsx
@@ -1,25 +1,85 @@
 import * as React from "react"
-import * as PopoverPrimitive from "@radix-ui/react-popover"
 import { cn } from "../../lib/utils"
 
-const Popover = PopoverPrimitive.Root
+const Popover = ({ children }) => {
+  const [open, setOpen] = React.useState(false)
+  
+  return (
+    <div className="relative">
+      {React.Children.map(children, child => 
+        React.cloneElement(child, { open, setOpen })
+      )}
+    </div>
+  )
+}
 
-const PopoverTrigger = PopoverPrimitive.Trigger
-
-const PopoverContent = React.forwardRef(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
-  <PopoverPrimitive.Portal>
-    <PopoverPrimitive.Content
+const PopoverTrigger = React.forwardRef(({ className, children, open, setOpen, asChild, ...props }, ref) => {
+  if (asChild) {
+    return React.cloneElement(children, {
+      ...props,
+      onClick: () => setOpen(!open)
+    })
+  }
+  
+  return (
+    <button
       ref={ref}
-      align={align}
-      sideOffset={sideOffset}
+      className={className}
+      onClick={() => setOpen(!open)}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+})
+PopoverTrigger.displayName = "PopoverTrigger"
+
+const PopoverContent = React.forwardRef(({ 
+  className, 
+  children, 
+  open, 
+  setOpen,
+  align = "start",
+  sideOffset = 4,
+  ...props 
+}, ref) => {
+  const contentRef = React.useRef(null)
+  
+  React.useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (contentRef.current && !contentRef.current.contains(event.target)) {
+        setOpen(false)
+      }
+    }
+    
+    if (open) {
+      document.addEventListener("mousedown", handleClickOutside)
+    }
+    
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [open, setOpen])
+  
+  if (!open) return null
+  
+  return (
+    <div
+      ref={contentRef}
       className={cn(
-        "z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        "absolute z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md",
+        align === "start" && "left-0",
+        align === "end" && "right-0",
+        align === "center" && "left-1/2 -translate-x-1/2",
+        "top-full mt-1",
         className
       )}
       {...props}
-    />
-  </PopoverPrimitive.Portal>
-))
-PopoverContent.displayName = PopoverPrimitive.Content.displayName
+    >
+      {children}
+    </div>
+  )
+})
+PopoverContent.displayName = "PopoverContent"
 
 export { Popover, PopoverTrigger, PopoverContent }
